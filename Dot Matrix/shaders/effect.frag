f6d6389cea5105e3dd9b1aff777380fdbda7e53b
Shaoting-Yan <112460516+Shaoting-Yan@users.noreply.github.com>
2023-09-19 01:25:00 +0800

    folder name change

--- Dot Matrix/shaders/effect.frag
+++ Dot Matrix/shaders/effect.frag
@@ -22,9 +22,18 @@ vec4 blur13(sampler2D image, vec2 uv, vec2 resolution, vec2 direction) {
   return color;
 }
 
+vec4 blur5(sampler2D image, vec2 uv, vec2 resolution, vec2 direction) {
+  vec4 color = vec4(0.0);
+  vec2 off1 = vec2(1.3333333333333333) * direction;
+  color += texture2D(image, uv) * 0.29411764705882354;
+  color += texture2D(image, uv + (off1 / resolution)) * 0.35294117647058826;
+  color += texture2D(image, uv - (off1 / resolution)) * 0.35294117647058826;
+  return color; 
+}
+
 void main() {
 
   vec2 uv = vec2(gl_FragCoord.xy / iResolution.xy);
-  gl_FragColor = blur13(tex0, uv, iResolution, direction);;
+  gl_FragColor = blur5(tex0, uv, iResolution, direction);;
 }
 
--- Dot Matrix/sketch.js
+++ Dot Matrix/sketch.js
@@ -4,8 +4,7 @@ var backSketch = function(p){
   p.he = 3;
   p.sw = 3;
   p.su = 2;
-  p.da = 1;
-  thickness = 35;
+  p.da = 10;
   p.preload = function(){
     p.blurShader = p.loadShader('shaders/effect.vert', 'shaders/effect.frag');
   }
@@ -17,136 +16,114 @@ var backSketch = function(p){
     p.viewSize = p.camHeight*Math.PI*2;
 
     //layer 1, the frontmost layer
-    layer1 = p.createGraphics(p.viewSize,p.height);
-    layer1Pass1 = p.createFramebuffer();
+    layer1 = p.createFramebuffer({ depth: false });//for some weird reason has to disable depth
+    layer1.resize(p.viewSize,p.viewSize);
+    layer1Pass1 = p.createFramebuffer({ depth: false });
     layer1Pass1.resize(layer1.width,layer1.height);
-    layer1Pass2 = p.createFramebuffer();
+    layer1Pass2 = p.createFramebuffer({ depth: false });
     layer1Pass2.resize(layer1.width,layer1.height);
     layer1.blurRadius = 0.1;//can not set this to zero
-    layer1.offset = 200;
-    layer1.space = 4;
+    layer1.offset = 160;
     layer1.size = p.int(layer1.width/layer1.offset);
-    layer1.arrows = [];
-    layer1.tagged = random1D(layer1.size,layer1.size/2,layer1.space);
-    for(let i = 0;i<layer1.tagged.length;i++){
-      let length = p.randomGaussian(layer1.height*0.75, layer1.height*0.25);
-      if(layer1.tagged[i]){
-        layer1.arrows.push(new ArrowCurve(0,length*(2/5),length*(3/5),length,thickness,"black"));
-      }else{
-        layer1.arrows.push(new ArrowCurve(0,length*(2/5),length*(3/5),length,thickness,"#DE3D83"));
+    layer1.begin();
+      p.clear();
+      p.translate(-layer1.width/2,-layer1.height/2);
+      for(let i = 0;i<layer1.size;i++){
+        for(let j = 0;j<layer1.size;j++){
+          p.push();
+          p.noStroke();
+          p.fill('#DE3D83');
+          p.translate(i*layer1.offset,j*layer1.offset);
+          p.circle(0,0,80);
+          p.pop();
+        }
       }
-    }
-
+    layer1.end();
+    
     //layer 2
-    layer2 = p.createGraphics(p.viewSize,p.height);
-    layer2Pass1 = p.createFramebuffer();
+    layer2 = p.createFramebuffer({ depth: false });//for some weird reason has to disable depth
+    layer2.resize(p.viewSize/2,p.viewSize/2);//resize to reducce buffer usage
+    layer2Pass1 = p.createFramebuffer({ depth: false });
     layer2Pass1.resize(layer2.width,layer2.height);
-    layer2Pass2 = p.createFramebuffer();
+    layer2Pass2 = p.createFramebuffer({ depth: false });
     layer2Pass2.resize(layer2.width,layer2.height);
-    layer2.blurRadius = 2;//can not set this to zero
-    layer2.offset = 200;
-    layer2.space = 4;
+    layer2.blurRadius = 1.5;//can not set this to zero
+    layer2.offset = 80;
     layer2.size = p.int(layer2.width/layer2.offset);
-    layer2.arrows = [];
-    layer2.tagged = random1D(layer2.size,layer2.size/2,layer2.space);
-    for(let i = 0;i<layer2.tagged.length;i++){
-      let length = p.randomGaussian(layer2.height*0.75, layer2.height*0.25);
-      if(layer2.tagged[i]){
-        layer2.arrows.push(new ArrowCurve(0,length*(2/5),length*(3/5),length,thickness,"#DE3D83"));
-      }else{
-        layer2.arrows.push(new ArrowCurve(0,length*(2/5),length*(3/5),length,thickness,"#DE3D83"));
+    layer2.begin();
+      p.clear();
+      p.translate(-layer2.width/2,-layer2.height/2);
+      for(let i = 0;i<layer2.size;i++){
+        for(let j = 0;j<layer2.size;j++){
+          p.push();
+          p.noStroke();
+          p.fill('#2677BB');
+          p.translate(i*layer2.offset,j*layer2.offset);
+          p.circle(0,0,35);
+          p.pop();
+        }
       }
-    }
+    layer2.end();
+
     //layer 3
-    layer3 = p.createGraphics(p.viewSize,p.height);
-    layer3Pass1 = p.createFramebuffer();
+    layer3 = p.createFramebuffer({ depth: false });//for some weird reason has to disable depth
+    layer3.resize(p.viewSize/2,p.viewSize/2);//resize to reducce buffer usage
+    layer3Pass1 = p.createFramebuffer({ depth: false });
     layer3Pass1.resize(layer3.width,layer3.height);
-    layer3Pass2 = p.createFramebuffer();
+    layer3Pass2 = p.createFramebuffer({ depth: false });
     layer3Pass2.resize(layer3.width,layer3.height);
-    layer3.blurRadius = 5;//can not set this to zero
-    layer3.offset = 200;
-    layer3.space = 4;
+    layer3.blurRadius = 1.5;//can not set this to zero
+    layer3.offset = 80;
     layer3.size = p.int(layer3.width/layer3.offset);
-    layer3.arrows = [];
-    layer3.tagged = random1D(layer3.size,layer3.size/2,layer3.space);
-    for(let i = 0;i<layer3.tagged.length;i++){
-      let length = p.randomGaussian(layer3.height*0.75, layer3.height*0.25);
-      if(layer3.tagged[i]){
-        layer3.arrows.push(new ArrowCurve(0,length*(2/5),length*(3/5),length,thickness,"black"));
-      }else{
-        layer3.arrows.push(new ArrowCurve(0,length*(2/5),length*(3/5),length,thickness,"#DE3D83"));
+    layer3.begin();
+      p.clear();
+      p.translate(-layer3.width/2,-layer3.height/2);
+      for(let i = 0;i<layer3.size;i++){
+        for(let j = 0;j<layer3.size;j++){
+          p.push();
+          p.noStroke();
+          p.fill('#00B8B8');
+          p.translate(i*layer3.offset,j*layer3.offset);
+          p.circle(0,0,25);
+          p.pop();
+        }
       }
-    }
+    layer3.end();
   }
   p.draw = function(){
     let rotation = getEulerAngles(getRotationMatrix(p.rotationZ,p.rotationX,p.rotationY));
-    p.Rz = Math.PI-rotation[0];
+    p.Rz = rotation[0];
     p.Ry = rotation[2];
     p.Rx = rotation[1];  
     p.clear();
     p.background(255);
-    let currX = p.camHeight*p.Ry; 
-    p.translate(currX,0,0); 
+    moveCamera(p);
+    turnV1 = p.createVector(Math.cos(p.noise(p.frameCount/100)*2*Math.PI),
+                            Math.sin(p.noise(p.frameCount/100)*2*Math.PI)).setMag(35);//turning
+    turnV2 = p.createVector(Math.cos(p.noise(p.frameCount/100+0.2)*2*Math.PI),
+                            Math.sin(p.noise(p.frameCount/100+0.2)*2*Math.PI)).setMag(50);//turning
+    accV1 = p.createVector(p.accX,p.accY).div(2);//accX, accY are from moveCamera function
+    accV2 = p.createVector(p.accX,p.accY);
 
+    moveObject(p);
 
-    layer1.clear();
-    layer1.push();
-    for(let i = 0;i<(layer1.arrows.length);i++){
-      layer1.push();
-      layer1.translate(i*layer1.offset,0);
-      let curr = layer1.arrows[i];
-      curr.display(layer1,p.Rz);
-      layer1.pop();
-    }
-    layer1.translate(layer1.width/2,0);
-    layer1.pop();
     p.gaussianBlur(layer1,layer1Pass1,layer1Pass2,layer1.blurRadius);
-
-    layer2.clear();
-    layer2.push();
-    for(let i = 0;i<(layer2.arrows.length);i++){
-      layer2.push();
-      layer2.translate(i*layer2.offset,0);
-      let curr = layer2.arrows[i];
-      curr.display(layer2,p.Rz);
-      layer2.pop();
-    }
-    layer2.translate(layer2.width/2,0);
-    layer2.pop();
     p.gaussianBlur(layer2,layer2Pass1,layer2Pass2,layer2.blurRadius);
-
-    layer3.clear();
-    layer3.push();
-    for(let i = 0;i<(layer3.arrows.length);i++){
-      layer3.push();
-      layer3.translate(i*layer3.offset,0);
-      let curr = layer3.arrows[i];
-      curr.display(layer3,p.Rz);
-      layer3.pop();
-    }
-    layer3.translate(layer3.width/2,0);
-    layer3.pop();
     p.gaussianBlur(layer3,layer3Pass1,layer3Pass2,layer3.blurRadius);
 
-
     p.resetShader();
-
     p.push();
     p.imageMode(p.CENTER);
     p.blendMode(p.MULTIPLY);
-    p.noStroke();
-    p.translate(0,0,-800);
-    let factor = (base,incr) => ((base+incr)/base);
-    let f1 = factor(p.camHeight,800);
-    p.image(layer3Pass2,0,0,layer3Pass2.width*f1,layer3Pass2.height*f1);
-    p.translate(0,0,400);
-    let f2 = factor(p.camHeight,400);
-    p.image(layer2Pass2,0,0,layer2Pass2.width*f2,layer2Pass2.height*f2);
-    p.translate(0,0,400);
+    sumV1 = p5.Vector.add(accV1, turnV1);
+    p.image(layer3Pass2,sumV1.x,sumV1.y,layer3Pass2.width*2,layer3Pass2.height*2);
+    p.translate(0,0,1);
+    sumV2 = p5.Vector.add(accV2, turnV2);
+    p.image(layer2Pass2,sumV2.x,sumV2.y,layer2Pass2.width*2,layer2Pass2.height*2);
+    p.translate(0,0,1);
     p.image(layer1Pass2,0,0);
+    // p.image(layer3Pass2,0,0);
     p.pop();
-    moveCamera(p);
-    // p.orbitControl();
   }
 
   p.gaussianBlur = function (source,pbo1,pbo2,radius){
@@ -156,6 +133,7 @@ var backSketch = function(p){
       //two pass gaussian blur
       pbo1.begin();
         p.clear();
+        p.noStroke();
         p.shader(p.blurShader);
         p.blurShader.setUniform('tex0', i==0?source:pbo2);
         p.blurShader.setUniform('direction', [0,spread]);
@@ -164,6 +142,7 @@ var backSketch = function(p){
       pbo1.end();
       pbo2.begin();
         p.clear();
+        p.noStroke();
         p.shader(p.blurShader);
         p.blurShader.setUniform('tex0', pbo1);
         p.blurShader.setUniform('direction', [spread,0]);
